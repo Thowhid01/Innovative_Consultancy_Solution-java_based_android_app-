4a52d6a40c9895e1cd4b91e05b0f68dc1fbc2048
Thowhid01 <bin123thowhid.com>
2022-04-18 14:59:32 +0600

    splash design

---
+++ app/src/main/java/com/efty/innovativeconsultancysolutions/Splash_Screen.java
@@ -0,0 +1,28 @@
+package com.efty.innovativeconsultancysolutions;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Splash_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Thread thread = new Thread() {
+            public void run() {
+                try {
+                    sleep(1000);
+                    Intent i = new Intent(Splash_Screen.this, MainActivity.class);
+                    startActivity(i);
+                    finish();
+                } catch (Exception e) {
+                    e.fillInStackTrace();
+                }
+            }
+        };
+        thread.start();
+    }
+}
